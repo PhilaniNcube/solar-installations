7ac8bda53a7a3242c62b75e18c4c0fda34146b0b
Philani Ncube <ncbphi001@gmail.com>
2023-09-12 15:33:56 +0200

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import { Toaster } from "@/components/ui/toaster";
 import Container from '@/components/Layout/Container'
 import InnerContent from '@/components/Layout/InnerContent'
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FCLC8KKJH9"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FCLC8KKJH9');
+        `}
+      </Script>
       <body className={inter.className}>
         {children}
         <Toaster />
---
+++ utils/analytics.js
@@ -0,0 +1,16 @@
+import Analytics from 'analytics';
+import googleTagManager from '@analytics/google-tag-manager';
+
+const GTM_CONTAINER_ID = "GTM-M2TSCHP3";
+
+const analytics = Analytics({
+  app: 'solar-installation', // Call this whatever you like.
+  plugins: [
+    googleTagManager({
+      containerId: GTM_CONTAINER_ID,
+      enabled: true,
+    }),
+  ],
+});
+
+export default analytics;
